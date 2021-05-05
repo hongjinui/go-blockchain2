27a98fdc80d98a96572406d5776f4d089af48ded
hongjinui <houago3@gmail.com>
2021-05-05 11:17:13 +0900

    20210505 part3 Implement DB persistence

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hongjinui/go-blockchain2
 
 go 1.15
+
+require github.com/boltdb/bolt v1.3.1
--- main.go
+++ main.go
@@ -8,17 +8,27 @@ import (
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	/*
+		bc.AddBlock("Send 1 BTC to Ivan")
+		bc.AddBlock("Send 2 more BTC to Ivan")
+	*/
 
-	for _, block := range bc.blocks {
+	bci := bc.Iterator()
+
+	for {
+
+		block := bci.Next()
+
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 
 		pow := NewProofOfWork(block)
 		fmt.Printf("PoW : %s\n", strconv.FormatBool(pow.Vaildate()))
-
 		fmt.Println()
+
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
 	}
 }
