631ceb0323686531a20ba0515cba7e723eee71e0
hongjinui <houago3@gmail.com>
2021-05-09 20:24:17 +0900

    20210509 part5 Implement Base59Decode

--- base58.go
+++ base58.go
@@ -1,6 +1,9 @@
 package main
 
-import "math/big"
+import (
+	"bytes"
+	"math/big"
+)
 
 var alphabet = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
@@ -29,4 +32,28 @@ func Base58Encode(input []byte) []byte {
 		}
 	}
 	return result
+}
+
+// Base58Decode decodes Base58 data
+func Base58Decode(input []byte) []byte {
+	result := big.NewInt(0)
+	zeroBytes := 0
+
+	for c := range input {
+		if c == 0x00 {
+			zeroBytes++
+		}
+	}
+
+	address := input[zeroBytes:]
+	for _, b := range address {
+		charIndex := bytes.IndexByte(alphabet, b)
+		result.Mul(result, big.NewInt(58))
+		result.Add(result, big.NewInt(int64(charIndex)))
+	}
+
+	raw := result.Bytes()
+	raw = append(bytes.Repeat([]byte{0x00}, zeroBytes), raw...)
+
+	return raw
 }
---
+++ wallet.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/gob"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+const version = byte(0x00)
+const walletFile = "wallet.dat"
+
+// Wallet
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  ecdsa.PublicKey
+}
+
+// GetAddress returns wallet address
+func (w Wallet) GetAddress() []byte {
+	public := append(w.PublicKey.X.Bytes(), w.PublicKey.Y.Bytes()...)
+
+	RIPEMD160Hasher := ripemd160.New()
+	_, err := RIPEMD160Hasher.Write(public)
+	if err != nil {
+		log.Panic(err)
+	}
+	publicRIPEMD160 := RIPEMD160Hasher.Sum(nil)
+
+	versionedPayload := append([]byte{version}, publicRIPEMD160...)
+	checksum := checksum(versionedPayload)
+
+	fullPayload := append(versionedPayload, checksum...)
+	address := Base58Encode(fullPayload)
+
+	return address
+}
+
+// SaveToFile saves the wallet to a file
+func (w Wallet) SaveToFile() {
+	var content bytes.Buffer
+
+	gob.Register(w.PrivateKey.Curve)
+	encoded := gob.NewEncoder(&content)
+	err := encoded.Encode(w)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// NewWallet creates and returns a Wallet
+func NewWallet() (*Wallet, error) {
+	if _, err := os.Stat(walletFile); !os.IsNotExist(err) {
+		return nil, errors.New("Wallet already exists")
+	}
+
+	private, public := newKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet, nil
+}
+
+func newKeyPair() (ecdsa.PrivateKey, ecdsa.PublicKey) {
+	curve := elliptic.P256()
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	return *private, private.PublicKey
+}
+
+// checksum
+func checksum(payload []byte) []byte {
+	firstSHA := sha256.Sum256(payload)
+	secondSHA := sha256.Sum256(firstSHA[:])
+
+	return secondSHA[:4]
+}
